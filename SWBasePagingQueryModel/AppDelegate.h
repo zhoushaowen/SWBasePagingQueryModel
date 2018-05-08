01dfc80a42b59462bcbdb77b615afbd5038c912d
zhoushaowen <348345883@qq.com>
2018-05-08 17:11:02 +0800

    初始化项目

---
+++ SWBasePagingQueryModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SWBasePagingQueryModel
+//
+//  Created by zhoushaowen on 2018/5/8.
+//  Copyright © 2018年 zhoushaowen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SWBasePagingQueryModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SWBasePagingQueryModel
+//
+//  Created by zhoushaowen on 2018/5/8.
+//  Copyright © 2018年 zhoushaowen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
